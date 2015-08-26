88d2e23a860c7910a560cf9b35e3566b9ebc55b4
rdinter <robert.dinterman@gmail.com>
2015-08-25 20:03:18 -0400

    Set-up Migration tidyr

--- 0-Data/0-IRS_Mig.R
+++ 0-Data/0-IRS_Mig.R
@@ -12,7 +12,8 @@ suppressMessages(library(readr))
 #Problem for excel files with "us" from 1998.99 until 2001.2
 read.excel1 <- function(file){
   data   <- read_excel(file)
-  data   <- data[c(8:nrow(data)), c(1:9)]
+  data   <- data[, c(1:9)]
+  #data   <- data[c(8:nrow(data)), c(1:9)]
   return(data)
 }
 read.excel2 <- function(file){
@@ -20,7 +21,10 @@ read.excel2 <- function(file){
   data   <- data[c(4:nrow(data)), c(1:9)]
   return(data)
 }
-
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
 # Create a directory for the data
 localDir <- "0-Data/IRS"
 data_source <- paste0(localDir, "/Raw")
@@ -45,126 +49,95 @@ if (all(sapply(files, function(x) !file.exists(x)))) {
 
 allindata  <- data.frame()
 alloutdata <- data.frame()
+
+namesi <- c("State_Code_Dest", "County_Code_Dest", "State_Code_Origin",
+            "County_Code_Origin", "State_Abbrv", "County_Name", "Return_Num",
+            "Exmpt_Num", "Aggr_AGI")
+nameso <- c("State_Code_Origin", "County_Code_Origin", "State_Code_Dest",
+            "County_Code_Dest", "State_Abbrv", "County_Name", "Return_Num",
+            "Exmpt_Num", "Aggr_AGI")
+
 for (i in files){  
   unzip(i, exdir = tempDir)
-  j5     <- list.files(paste0(tempDir))
-  j6     <- list.files(paste0(tempDir, "/", j5))
-  j5i    <- list.files(paste0(tempDir, "/", j5, "/", j6[1]))
-  j5o    <- list.files(paste0(tempDir, "/", j5, "/", j6[2]))
-  namesi <- c("State_Code_Dest", "County_Code_Dest", "State_Code_Origin",
-              "County_Code_Origin", "State_Abbrv", "County_Name", "Return_Num",
-              "Exmpt_Num", "Aggr_AGI")
-  indata <- data.frame()
-  for (j in j5i){
-    file   <- paste0(tempDir, "/", j5[1], "/", j6[1], "/", j)
-    
-    data <- read.excel2(file)
-    #data <- tryCatch(read.excel1(file), error = function(e){
-    # read.excel2(file)
-    #})
+  j5  <- list.dirs(tempDir)
+  j5i <- list.files(j5[grepl("Inflow", j5)], full.names = T)
+  j5o <- list.files(j5[grepl("Outflow", j5)], full.names = T)
+  
+  indata <- sapply(j5i, function(x){
+    data <- tryCatch(read.excel1(x), error = function(e){
+      read.excel2(x)
+    })
     
     data[,c(1:4,7:9)] <- lapply(data[,c(1:4,7:9)],
-                                function(x){ # Sometimes characters in values
+                                function(xx){ # Sometimes characters in values
                                   as.numeric(
                                     gsub(",", "", 
-                                         gsub("[A-z]", "", x)))
+                                         gsub("[A-z]", "", xx)))
                                 })
-    data[,c(5:6)]     <- lapply(data[,c(5:6)], function(x){as.character(x)})
+    data[,c(5:6)]     <- lapply(data[,c(5:6)], function(xx){as.character(xx)})
     names(data)       <- namesi
-    data$year  <- as.numeric(substr(basename(i), 1, 4))
+    
+    data <- filter(data, !is.na(State_Code_Dest))
+    data$State_Code_Dest <- Mode(data$State_Code_Dest)
+    # data$year  <- as.numeric(substr(basename(x), 1, 4))
     data$ofips <- data$State_Code_Origin*1000 + data$County_Code_Origin
     data$dfips <- data$State_Code_Dest*1000   + data$County_Code_Dest
-    indata     <- bind_rows(indata, data)
-    
-    print(paste0("Finished ", basename(j), " at ", Sys.time()))
-    
-  }
+    data
+  }, simplify = F, USE.NAMES = T)
+  indata <- bind_rows(indata)
+  indata$year <- as.numeric(substr(basename(i), 1, 4))
+  
   bfile <- gsub('.{4}$', '', basename(i))
-  write_csv(indata, paste0(localDir, "/", bfile,"i.csv"))
+  # write_csv(indata, paste0(localDir, "/", bfile,"i.csv"))
   
   
-  nameso <- c("State_Code_Origin", "County_Code_Origin", "State_Code_Dest",
-              "County_Code_Dest", "State_Abbrv", "County_Name", "Return_Num",
-              "Exmpt_Num", "Aggr_AGI")
-  outdata <- data.frame()
-  for (j in j5o){
-    file   <- paste0(tempDir, "/", j5[1], "/", j6[2], "/", j)
-    
-    data <- read.excel2(file)
-    #data <- tryCatch(read.excel1(file), error = function(e){
-    # read.excel2(file)
-    #})
+  outdata <- sapply(j5o, function(x){
+    data <- tryCatch(read.excel1(x), error = function(e){
+      read.excel2(x)
+    })
     
     data[,c(1:4,7:9)] <- lapply(data[,c(1:4,7:9)],
-                                function(x){ # Sometimes characters in values
+                                function(xx){ # Sometimes characters in values
                                   as.numeric(
                                     gsub(",", "", 
-                                         gsub("[A-z]", "", x)))
+                                         gsub("[A-z]", "", xx)))
                                 })
-    data[,c(5:6)]     <- lapply(data[,c(5:6)], function(x){as.character(x)})
+    data[,c(5:6)]     <- lapply(data[,c(5:6)], function(xx){as.character(xx)})
     names(data)       <- nameso
-    data$year   <- as.numeric(substr(basename(i), 1, 4))
+    
+    data <- filter(data, !is.na(State_Code_Origin))
+    data$State_Code_Origin <- Mode(data$State_Code_Origin)
+    # data$year   <- as.numeric(substr(basename(i), 1, 4))
     data$ofips  <- data$State_Code_Origin*1000 + data$County_Code_Origin
     data$dfips  <- data$State_Code_Dest*1000   + data$County_Code_Dest
-    outdata     <- rbind(outdata, data)
-    
-    print(paste0("Finished ", basename(j), " at ", Sys.time()))
-  }  
-  write_csv(outdata, paste0(localDir, "/", bfile,"o.csv"))
+    data
+  }, simplify = F, USE.NAMES = T)
+  outdata <- bind_rows(outdata)
+  outdata$year <- as.numeric(substr(basename(i), 1, 4))
+  
+  # write_csv(outdata, paste0(localDir, "/", bfile,"o.csv"))
   
   unlink(tempDir, recursive = T)
   allindata  <- bind_rows(allindata, indata)
   alloutdata <- bind_rows(alloutdata, outdata)
+  rm(indata, outdata)
   print(paste0("Finished ", basename(i), " at ", Sys.time()))
 }
-write_csv(allindata, paste0(localDir, "/inflows9204.csv"))
-write_csv(alloutdata, paste0(localDir, "/outflows9204.csv"))
+write_csv(allindata, paste0(localDir, "/inflows9203.csv"))
+write_csv(alloutdata, paste0(localDir, "/outflows9203.csv"))
 
-rm(allindata, alloutdata)
 
-# Data from 2004 to 2010
+# ---- Data from 2004 to 2012 ---------------------------------------------
+
 inflows  <- c("countyinflow0405.csv", "countyinflow0506.csv",
               "countyinflow0607.csv", "countyinflow0708.csv",
               "countyinflow0809.csv", "countyinflow0910.csv",
-              "countyinflow1011.csv")
-indata   <- data.frame()
-for (i in inflows){
-  file       <- paste0(data_source, "/", i)
-  if (!file.exists(file)) (download.file(paste0(url, i), file))
-  data       <- read_csv(file)
-  data$year  <- 1999 + as.numeric(substr(i, nchar(i) - 5, nchar(i) - 4))
-  data$ofips <- data$State_Code_Origin*1000 + data$County_Code_Origin
-  data$dfips <- data$State_Code_Dest*1000   + data$County_Code_Dest
-  indata     <- bind_rows(indata, data)
-  print(paste0("Finished ", basename(i), " at ", Sys.time()))
-  
-}
-write_csv(indata, paste0(localDir, "/inflows0410.csv"))
+              "countyinflow1011.csv", "countyinflow1112.csv",
+              "countyinflow1213.csv")
 
-outflows <- c("countyoutflow0405.csv", "countyoutflow0506.csv",
-              "countyoutflow0607.csv", "countyoutflow0708.csv",
-              "countyoutflow0809.csv", "countyoutflow0910.csv",
-              "countyoutflow1011.csv")
-outdata  <- data.frame()
-for (i in outflows){
-  file       <- paste0(data_source, "/", i)
-  if (!file.exists(file)) (download.file(paste0(url, i),file))
-  data       <- read_csv(file)
-  data$year  <- 1999 + as.numeric(substr(i, nchar(i) - 5, nchar(i) - 4))
-  data$ofips <- data$State_Code_Origin*1000 + data$County_Code_Origin
-  data$dfips <- data$State_Code_Dest*1000   + data$County_Code_Dest
-  outdata    <- bind_rows(outdata, data)
-  print(paste0("Finished ", basename(i), " at ", Sys.time()))
-  
-}
-write_csv(outdata, paste0(localDir, "/outflows0410.csv"))
-
-# Data from 2011 to 2012
-inflows  <- c("countyinflow1112.csv", "countyinflow1213.csv")
-indata   <- data.frame()
-for (i in inflows){
-  file       <- paste0(data_source, "/", i)
-  if (!file.exists(file)) (download.file(paste0(url, i), file))
+indata   <- sapply(inflows, function(x){
+  file       <- paste0(data_source, "/", x)
+  if (!file.exists(file)) (download.file(paste0(url, x), file))
   data       <- read_csv(file, col_names = c("State_Code_Dest",
                                              "County_Code_Dest",
                                              "State_Code_Origin",
@@ -172,35 +145,48 @@ for (i in inflows){
                                              "State_Abbrv", "County_Name",
                                              "Return_Num", "Exmpt_Num",
                                              "Aggr_AGI"), skip = 1)
-  data$year  <- 1999 + as.numeric(substr(i, nchar(i) - 5, nchar(i) - 4))
+  data$year  <- 1999 + as.numeric(substr(x, nchar(x) - 5, nchar(x) - 4))
   data$ofips <- data$State_Code_Origin*1000 + data$County_Code_Origin
   data$dfips <- data$State_Code_Dest*1000   + data$County_Code_Dest
-  indata     <- bind_rows(indata, data)
-  print(paste0("Finished ", basename(i), " at ", Sys.time()))
-  
-}
-write_csv(indata, paste0(localDir, "/inflows1113.csv"))
+  filter(data, !is.na(State_Code_Dest))
+}, simplify = F, USE.NAMES = T)
 
-outflows <- c("countyoutflow1112.csv", "countyoutflow1213.csv")
-outdata  <- data.frame()
-for (i in outflows){
-  file       <- paste0(data_source, "/", i)
-  if (!file.exists(file)) (download.file(paste0(url, i),file))
+allin <- bind_rows(indata)
+
+write_csv(allin, paste0(localDir, "/inflows0413.csv"))
+
+outflows <- c("countyoutflow0405.csv", "countyoutflow0506.csv",
+              "countyoutflow0607.csv", "countyoutflow0708.csv",
+              "countyoutflow0809.csv", "countyoutflow0910.csv",
+              "countyoutflow1011.csv", "countyoutflow1112.csv",
+              "countyoutflow1213.csv")
+outdata  <- sapply(inflows, function(x){
+  file       <- paste0(data_source, "/", x)
+  if (!file.exists(file)) (download.file(paste0(url, x), file))
   data       <- read_csv(file, col_names = c("State_Code_Origin",
                                              "County_Code_Origin",
                                              "State_Code_Dest",
                                              "County_Code_Dest", "State_Abbrv",
                                              "County_Name", "Return_Num",
                                              "Exmpt_Num", "Aggr_AGI"), skip = 1)
-  data$year  <- 1999 + as.numeric(substr(i, nchar(i) - 5, nchar(i) - 4))
+  data$year  <- 1999 + as.numeric(substr(x, nchar(x) - 5, nchar(x) - 4))
   data$ofips <- data$State_Code_Origin*1000 + data$County_Code_Origin
   data$dfips <- data$State_Code_Dest*1000   + data$County_Code_Dest
-  outdata    <- bind_rows(outdata, data)
-  print(paste0("Finished ", basename(i), " at ", Sys.time()))
-  
-}
-write_csv(outdata, paste0(localDir, "/outflows1113.csv"))
+  filter(data, !is.na(State_Code_Origin))
+}, simplify = F, USE.NAMES = T)
 
-rm(data, indata, outdata)
+allout <- bind_rows(outdata)
+
+write_csv(allout, paste0(localDir, "/outflows0413.csv"))
+
+rm(indata, outdata)
+
+allindata  <- bind_rows(allindata, allin)
+alloutdata <- bind_rows(alloutdata, allout)
+
+save(allindata,  file = paste0(localDir, "/inflows9213.Rda"))
+save(alloutdata, file = paste0(localDir, "/outflows9213.Rda"))
+
+rm(list = ls())
 
 print(paste0("Finished 0-IRS_Mig at ", Sys.time()))
---
+++ 1-Organization/1-Migration_Tidy.R
@@ -0,0 +1,38 @@
+# Robert Dinterman
+
+print(paste0("Started 1-Migration_Tidy at ", Sys.time()))
+
+suppressMessages(library(dplyr))
+suppressMessages(library(maptools))
+suppressMessages(library(readr))
+suppressMessages(library(tidyr))
+
+# Create a directory for the data
+localDir <- "1-Organization/Migration"
+if (!file.exists(localDir)) dir.create(localDir)
+
+load("0-Data/IRS/inflows9213.Rda")
+load("0-Data/IRS/outflows9213.Rda")
+load("0-Data/Shapefiles/All_2010_county.Rda")
+
+# Need to make sure to change the -1 to NA in the flows data ...
+
+# Next step is to fix any county issues ... new counties and merged counties!
+
+# Think about how to aggregate all of them ... they need to be in long form
+#  Do I take an average? Sure! 
+
+# write_csv(DATA, paste0(localDir, "/Migration.csv"))
+# save(DATA, file = paste0(localDir, "/Migration.Rda"))
+
+
+# Merge all of the control variables needed in this setting! Likely as a
+#  separate file and saved, but then as a totally massive freaking data.frame
+#  with Ocontrols and Dcontrols
+
+# write_csv(data, paste0(localDir, "/Final.csv"))
+# save(data, file = paste0(localDir, "/Final.Rda"))
+
+rm(list = ls())
+
+print(paste0("Finished 1-Migration_Tidy at ", Sys.time()))
