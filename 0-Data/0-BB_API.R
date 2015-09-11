0a41d82d31f8d53719b6933566cb8ad5e8fd8346
rdinter <robert.dinterman@gmail.com>
2015-09-11 15:00:53 -0400

    Ignores

---
+++ 0-Data/0-BB_API.R
@@ -0,0 +1,18 @@
+# Robert Dinterman
+# http://www.broadbandmap.gov/developer
+
+print(paste0("Started 0-BB_API at ", Sys.time()))
+
+options(scipen=999) #Turn off scientific notation for write.csv()
+# suppressMessages(library(dplyr))
+# suppressMessages(library(readr))
+# suppressMessages(library(stringr))
+# suppressMessages(library(tidyr))
+# source("0-Data/0-functions.R")
+
+
+# Create a directory for the data
+localDir <- "0-Data/Broadband_Map"
+data_source <- paste0(localDir, "/Raw")
+if (!file.exists(localDir)) dir.create(localDir)
+if (!file.exists(data_source)) dir.create(data_source)
