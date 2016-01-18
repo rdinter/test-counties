7197c80770867db7e65f97c23e998aad208f4eec
rdinter <robert.dinterman@gmail.com>
2016-01-18 13:39:16 -0500

    First pass of migration controls.

---
+++ 1-Organization/1-Migration-Controls_Tidy.R
@@ -0,0 +1,57 @@
+# Robert Dinterman
+
+# ---- Start --------------------------------------------------------------
+
+print(paste0("Started 1-Migration-Controls_Tidy at ", Sys.time()))
+
+library(dplyr)
+library(readr)
+library(tidyr)
+
+# Create a directory for the data
+localDir <- "1-Organization/Migration"
+if (!file.exists(localDir)) dir.create(localDir)
+
+load("1-Organization/Migration/netmigration.Rda")
+
+# ---- Controls -----------------------------------------------------------
+load("0-Data/IRS/CTYPop.Rda") # Population, Households, Income for county
+ctydata <- left_join(aggdata, IRS_POP)
+rm(IRS_POP)
+
+load("0-Data/CBP/CBPimpute.Rda") # County Business Patterns, establishments
+ctydata <- left_join(ctydata, cbpimpute[, c("year", "fips", "est")])
+rm(cbpimpute)
+
+load("0-Data/LAU/LAUnemp.RData")
+ctydata <- left_join(ctydata, unemp)
+rm(unemp)
+
+load("0-Data/ERS/ERS.Rda") # Problem with the different years ??
+temp <- expand.grid(unique(ERS$fips), 1990:2010)
+names(temp) <- c("fips", "year")
+temp <- left_join(temp, ERS)
+
+temp %>% select(-(metro93:metro13)) %>% 
+  group_by(fips) %>% do(zoo::na.locf(.)) -> temp
+
+ctydata <- left_join(ctydata, temp)
+rm(temp, ERS)
+
+load("0-Data/Poverty/pov.Rda") # Poverty Data w/ MEDHHINC
+ctydata <- left_join(ctydata, pov)
+rm(pov)
+
+load("0-Data/NOAA/noaabasic.Rda") # Storm Data
+noaabasic %>% rename(year = YEAR) %>% 
+  left_join(ctydata, .) -> ctydata
+
+rm(noaabasic)
+
+write_csv(ctydata, paste0(localDir, "/Aggctydata.csv"))
+save(ctydata, file = paste0(localDir, "/Aggctydata.Rda"))
+
+
+rm(list = ls())
+
+print(paste0("Finished 1-Migration-Controls_Tidy at ", Sys.time()))
